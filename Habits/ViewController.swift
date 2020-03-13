e4500ac4eba61149c80be97215f5410c94caf65f
Mario Hahn <mario_hahn@me.com>
2020-03-13 19:33:09 +0100

    Initial Project Setup

---
+++ Habits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habits
+//
+//  Created by Mario Hahn on 13.03.20.
+//  Copyright © 2020 Mario Hahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
